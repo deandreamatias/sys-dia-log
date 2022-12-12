51f737ac898fe812b41e1935778515478f6e0be1
Nickolay Petrash <nickolay_petrash@epam.com>
2022-12-12 16:34:59 +0300

    home-view, i18n, run

--- lib/modules/home/views/home_view.dart
+++ lib/modules/home/views/home_view.dart
@@ -1,13 +1,49 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   static const String routeName = "/";
 
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: I18nText('title'),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(
+            icon: const Icon(Icons.list_rounded),
+            label: FlutterI18n.translate(context, 'home'),
+          ),
+          NavigationDestination(
+              icon: const Icon(Icons.add_circle_outline_rounded),
+              label: FlutterI18n.translate(context, 'measurement')),
+          NavigationDestination(
+              icon: const Icon(Icons.settings_outlined),
+              label: FlutterI18n.translate(context, 'settings'))
+        ],
+        onDestinationSelected: (value) => setState(() {
+          currentPageIndex = value;
+        }),
+        selectedIndex: currentPageIndex,
+      ),
+      body: <Widget>[
+        Container(
+          color: Colors.blue,
+          alignment: Alignment.center,
+          child: const Text("blue"),
+        )
+      ][currentPageIndex],
+    );
   }
 }
