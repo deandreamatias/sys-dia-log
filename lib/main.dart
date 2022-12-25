a9dcb7494759d8ef3140f63b34dd367bb47528cb
Nickolay Petrash <nickolay_petrash@epam.com>
2022-12-25 23:46:53 +0300

    Added Hive DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'app.dart';
 
 /// Sys Dia Log application entrypoint.
 void main() async {
+  await Hive.initFlutter();
+
+  await Future.wait([
+    Hive.openBox('measurements'),
+  ]);
+
   runApp(SysDiaLogApp());
 }
