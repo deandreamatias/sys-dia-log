5ca52b259588420cbd7320325b332a12df052a83
darkowl91 <darkowl91@gmail.com>
2023-03-04 15:01:53 +0300

    cleanup

--- lib/modules/measurement/service/measurement_service.dart
+++ lib/modules/measurement/service/measurement_service.dart
@@ -3,20 +3,15 @@ import 'package:sys_dia_log/hive/hive_box.dart';
 import 'package:sys_dia_log/modules/measurement/models/measurement.dart';
 import 'package:sys_dia_log/modules/measurement/service/blood_pressure_category_calc.dart';
 
-import '../models/blood_pressure_category.dart';
-
 class MeasurementService {
   final BloodPressureCategoryCalc categoryCalc = BloodPressureCategoryCalc();
   final Box<Measurement> box = Hive.box(measurementsBox);
 
   Measurement createNewMeasurement(int sys, int dia, int bpm) {
-    BloodPressureCategory category =
-        categoryCalc.getCategoryByValues(systolic: sys, diastolic: dia);
-
-    Measurement measurement = Measurement.values(
+    final measurement = Measurement.values(
       systolic: sys,
       diastolic: dia,
-      category: category,
+      category: categoryCalc.getCategoryByValues(systolic: sys, diastolic: dia),
       bpm: bpm,
     );
 
